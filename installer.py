389ed2c2d95cd1010ec84a5c9025beea5f8c594d
khaledboka <bokae97@gmail.com>
2017-06-12 11:55:32 +0200

    Title, README.md

--- installer.py
+++ installer.py
@@ -1,7 +1,7 @@
 __author__ = 'cartologic'
 
 info = {
-    "title": "Point In Polygon Statistics",
+    "title": "Point In Polygon Analysis Tool",
     "description": "Computes statistics for the distribution of a given attribute in a set of polygonal zones for point layer & save the result in a new geonode/geoserver layer.",
     "author": 'Cartologic',
     "home_page": 'http://cartoview.org/apps/cartoview_point_in_polygon',
