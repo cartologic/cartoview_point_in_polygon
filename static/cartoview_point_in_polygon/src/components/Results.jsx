1872b6ac1bca3cfeeca036267257acbd8c6f00fe
khaledboka <bokae97@gmail.com>
2017-07-11 12:47:03 +0200

    result page redesigned

--- static/cartoview_point_in_polygon/src/components/Results.jsx
+++ static/cartoview_point_in_polygon/src/components/Results.jsx
@@ -1,10 +1,34 @@
 import { Component } from 'react';
-import GenerateLayer from './GenerateLayer.jsx'
 
 // using reactstrap Forms
-import { Alert, Link, Button, } from 'reactstrap';
+import { Alert } from 'reactstrap';
 
 export default class Results extends Component {
+  note(){
+    return(
+      <div className="panel panel-info">
+        <div className="panel-heading">Process Successfully Completed</div>
+        <div className="panel-body">
+          <div className="row">
+            <div className="col-xs-12 col-md-12">
+              The result of this process has been published as a portal layer. You can view the result and further configure the properties and set permission of the resulting Layer:
+              <span style={{fontWeight:"bold"}}>
+                {this.props.typeName.split(':').pop()}
+              </span>
+            </div>
+          </div>
+
+          <div className="row" style={{marginTop:"3%"}}>
+            <div className="col-xs-10 col-md-3 col-md-offset-9 col-xs-offset-1">
+              <a className="btn btn-primary" href={`/layers/${this.props.typeName}`} style={{float:"right"}} role="button">Layer Details</a>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    )
+  }
+
 
   render(){
     const {successState, typeName, loading} = this.props;
@@ -15,21 +39,22 @@ export default class Results extends Component {
     else{
       // Success !!!
       if(successState){
-        return <div>
-            <Alert  style={{margin: "10% auto auto"}} color="success">
-              Layer Created Successfully
-            </Alert>
-            <br></br>
-            <a className="btn btn-primary" href={`/layers/${typeName}`} style={{float:"right"}} role="button">Layer Details</a>
-          </div>;
+        return this.note()
       }
 
       // Failure !!!
       if(!successState){
         return (
-          <Alert style={{margin: "10% auto auto"}} color="danger">
-            Faild to Create layer
-          </Alert>
+          <div className="panel panel-danger">
+            <div className="panel-heading">Process Faild</div>
+            <div className="panel-body">
+              <div className="row">
+                <div className="col-xs-12 col-md-12">
+                  {"Error causes will be shown here"}
+                </div>
+              </div>
+            </div>
+          </div>
         )
       }
     }
--- static/cartoview_point_in_polygon/src/point-in-polygon.jsx
+++ static/cartoview_point_in_polygon/src/point-in-polygon.jsx
@@ -5,7 +5,7 @@ import AboutPage from './components/AboutPage.jsx'
 import LayersList from './components/LayersList.jsx'
 import Navigator from './components/Navigator.jsx'
 import AttributeSelector from './components/AttributeSelector.jsx'
-import NewLayerName from './components/LayerStyles.jsx'
+import NewLayerName from './components/NewLayerName.jsx'
 import Results from './components/Results.jsx'
 
 import "../css/styler.css";
