574d65af413849253fc80805f51d96b996db0450
khaledboka <bokae97@gmail.com>
2017-07-16 13:34:26 +0200

    Modal help instead of about page

---
+++ static/cartoview_point_in_polygon/src/constants/constants.jsx
@@ -0,0 +1,20 @@
+const DefaultModalStyle = {
+  overlay: {
+    position: 'fixed',
+    top: 80,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    // backgroundColor: 'rgba(255,255,255,0.20)',
+    zIndex: 999
+  },
+  content: {
+    outline: 'none',
+    // width: "80%",
+    height: '700px',
+    overflow: 'scroll',
+    // margin: 'auto',
+  }
+}
+
+export {DefaultModalStyle}
--- static/cartoview_point_in_polygon/src/point-in-polygon.jsx
+++ static/cartoview_point_in_polygon/src/point-in-polygon.jsx
@@ -8,6 +8,10 @@ import AttributeSelector from './components/AttributeSelector.jsx'
 import NewLayerName from './components/NewLayerName.jsx'
 import Results from './components/Results.jsx'
 
+import {DefaultModalStyle} from './constants/constants.jsx'
+import Modal from 'react-modal';
+
+
 import "../css/styler.css";
 
 class ConfigForm extends Component {
@@ -15,7 +19,9 @@ class ConfigForm extends Component {
     config: {outputLayerName: ""},
     step: 0,
     saved: false,
-    loading: true
+    loading: true,
+
+    modalIsOpen:false
   }
 
 
@@ -60,11 +66,61 @@ class ConfigForm extends Component {
     )
   }
 
+
+  helpModal(){
+    return(
+      <Modal className="modal-dialog"
+        isOpen={this.state.modalIsOpen}
+        style={DefaultModalStyle}
+        onRequestClose={()=>{this.setState({modalIsOpen: false})}}>
+        <div className="">
+          <div className="panel panel-default">
+            <div className="panel-heading">
+              <div className="row">
+                <div className="col-xs-6 col-md-6">
+                  {this.aboutHeader()}
+                </div>
+                <div className="col-xs-1 col-md-1 col-md-offset-5 col-xs-offset-5">
+                  <div className="pull-right">
+                    <a className="btn btn btn-primary"
+                      onClick={(e) =>{ e.preventDefault(); this.setState({modalIsOpen:false}) } }>
+                      x
+                    </a>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="panel-body">
+              <div className="row">
+                <div className="col-md-12">
+                  {this.aboutBody()}
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    )
+  }
+
+
   navBar(){
     return(
     <nav className="navbar navbar-default">
       <div className="container">
-        <h4 style={{color:"dimgray"}}>Point in Polygon Analysis Tool</h4>
+        <div className="row">
+          <div className="col-xs-6 col-md-6">
+            <h4 style={{color:"dimgray"}}>Point in Polygon</h4>
+          </div>
+          <div className="col-xs-1 col-md-1 col-md-offset-5 col-xs-offset-4">
+            <button type="button"
+              style={{marginTop:"8%"}}
+              className="btn btn-primary"
+              onClick={()=>{this.setState({modalIsOpen: true})}}>
+              ?
+            </button>
+          </div>
+        </div>
       </div>
     </nav>
     )
@@ -74,14 +130,6 @@ class ConfigForm extends Component {
   render() {
     var {config, step, saved} = this.state
     const steps = [{
-      label: "About",
-      component: AboutPage,
-      props: {
-        onComplete: () => this.updateConfig({}),
-        aboutHeader: this.aboutHeader(),
-        aboutBody: this.aboutBody()
-      }
-    },{
       label: "Select Point Layer",
       component: LayersList,
       props: {
@@ -139,6 +187,7 @@ class ConfigForm extends Component {
 
     return  (
       <div className="col-md-12">
+        {this.helpModal()}
         <div className="row">{this.navBar()}</div>
         <div className="row">
           <Navigator steps={steps} step={step} onStepSelected={(step)=>this.goToStep(step)}/>
