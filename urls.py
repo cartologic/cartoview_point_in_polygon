061881ecbe01841d368eb5c0cae278315e0f274b
hisham waleed karam <hisham.karam@cartologic.com>
2018-11-21 10:33:53 +0200

    django 1.11 compatibility

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 import views
 from . import APP_NAME
 from api import LayerResource
@@ -8,11 +8,12 @@ from tastypie.api import Api
 Resources_api = Api(api_name="api")
 Resources_api.register(LayerResource())
 
-urlpatterns = patterns('',
+urlpatterns = [
     # /APP_NAME
     url(r'^$', views.index, name='%s.index' % APP_NAME),
     # generates layer from geoserver | /APP_NAME/generate-layer
-    url(r'^generate-layer$', views.generate_layer, name='%s.generate_layer' % APP_NAME),
+    url(r'^generate-layer$', views.generate_layer,
+        name='%s.generate_layer' % APP_NAME),
     #
     url(r'^', include(Resources_api.urls)),
-)
+]
